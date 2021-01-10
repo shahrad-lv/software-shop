c17483421171d06851a80f9f9d5a7c0642886ae6
shahrad <shahrad,king1380@gmail,com>
2021-01-10 20:25:19 +0330

    Shop Component V9 - add Alert & Confirm Dialog

--- src/components/layout/Cart/CartProduct.jsx
+++ src/components/layout/Cart/CartProduct.jsx
@@ -4,29 +4,84 @@ import { ProductContent, ProductPic, ProductContainer, ProductDelete,ProductName
 import DeleteIcon from '@material-ui/icons/Delete';
 import { removeCart, Increase, Decrease } from "../../../redux/Actions/cartAction";
 import { ButtonGroup } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 const CartProduct = ({product}) => {
+
+  const [openRemove, setOpenRemove] = useState(false);
+  const handleClickOpenRemove = () => {
+    setOpenRemove(true);
+  };
+  const handleCloseRemove = () => {
+    setOpenRemove(false);
+  };
 
   const dispatch = useDispatch();
   const handleRemove = (product) => {
+    setOpenRemove(false);
     dispatch(removeCart(product))
   }
+
+  const [openAlert, setOpenAlert] = useState(false);
+  const handleCloseAlert = () => {
+      setOpenAlert(false);
+  };
 
   const handleIncrease = (product) => {
     product.stock > product.Count ?
     dispatch(Increase(product))
-    : alert('در انبار موجود نمی باشد')
+    : setOpenAlert(true);
   }
   
   const handleDecrease = (product) => {
     product.Count > 1 ?
     dispatch(Decrease(product))
-    : alert('تعداد نمی تواند کم تر از 1 باشد')
+    : setOpenRemove(true)
   }
 
 
 
     return (
+      <>
+      <Dialog open={openRemove} TransitionComponent={Transition} keepMounted onClose={handleCloseRemove}>
+        <DialogTitle>حذف محصول</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            آیا مایل به حذف این محصول هستید؟ 
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary" onClick={handleCloseRemove}>
+            انصراف
+          </Button>
+          <Button  color="primary" onClick={() => handleRemove(product)} >
+            تایید
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={openAlert} TransitionComponent={Transition} keepMounted onClose={handleCloseAlert}>
+        {/* <DialogTitle>حذف محصول</DialogTitle> */}
+        <DialogContent>
+          <DialogContentText>
+            در انبار موجود نمی باشد
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button  color="primary" onClick={handleCloseAlert} >
+            تایید
+          </Button>
+        </DialogActions>
+      </Dialog>
       <ProductContainer>
         <ProductContent>
           <ProductName>{product.ProductName}</ProductName>
@@ -37,13 +92,15 @@ const CartProduct = ({product}) => {
               <ProductIncrease onClick={() => handleIncrease(product)}>+</ProductIncrease>
               <ProductDecrease onClick={() => handleDecrease(product)}>-</ProductDecrease>
             </ButtonGroup>
-            <ProductDelete variant="outlined" color="secondary" onClick={() => handleRemove(product)} endIcon={<DeleteIcon />}>
+            <ProductDelete variant="outlined" color="secondary" onClick={handleClickOpenRemove}  endIcon={<DeleteIcon />}>
               حذف
             </ProductDelete>
           </ProductActions>
         </ProductContent>
         <ProductPic src={product.ProductPic} />
       </ProductContainer>
+      </>
+
     )
 }
 
--- src/components/layout/Products/Product.jsx
+++ src/components/layout/Products/Product.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { AddToCart ,InfoName, InfoPrice, ProductContainer, ProductImg, ProductInfo } from './Product.elements';
 import { addCart } from "../../../redux/Actions/productAction";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Product = ({product}) => {
 
+  const [openAlert, setOpenAlert] = useState(false);
+  const handleCloseAlert = () => {
+      setOpenAlert(false);
+  };
+
   const dispatch = useDispatch();
   const handleAdd = product => {
+    product.stock > product.Count ?
     dispatch(addCart(product))
+    : setOpenAlert(true);
   }
   
   return (
-    <ProductContainer>
-        <ProductImg src={product.ProductPic} alt=""/>
-      <ProductInfo>
-      <InfoPrice>
-           {product.ProductPrice} تومان
-        </InfoPrice>
-        <InfoName>
-          {product.ProductName}
-        </InfoName>
-        </ProductInfo>
-        <AddToCart onClick={() => handleAdd(product)} variant='outlined' color='secondary'> افزودن به سبد خرید</AddToCart>
-    </ProductContainer>
+    <>
+      <Dialog open={openAlert} TransitionComponent={Transition} keepMounted onClose={handleCloseAlert}>
+        {/* <DialogTitle>حذف محصول</DialogTitle> */}
+        <DialogContent>
+          <DialogContentText>
+            در انبار موجود نمی باشد
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button  color="primary" onClick={handleCloseAlert} >
+            تایید
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <ProductContainer>
+          <ProductImg src={product.ProductPic} alt=""/>
+        <ProductInfo>
+        <InfoPrice>
+            {product.ProductPrice} تومان
+          </InfoPrice>
+          <InfoName>
+            {product.ProductName}
+          </InfoName>
+          </ProductInfo>
+          <AddToCart onClick={() => handleAdd(product)} variant='outlined' color='secondary'> افزودن به سبد خرید</AddToCart>
+      </ProductContainer>
+    </>
+
   );
 }
 
